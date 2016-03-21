a9fdf2d7df9777288aa534df4c491fa78ef53320
lxh <86849081@qq.com>
2016-03-21 09:57:28 +0800

    java lru

--- lect09/LRU.java
+++ lect09/LRU.java
@@ -5,19 +5,16 @@ public class LRU {
     public static void main(String[] args) {
         List<Integer> pages = new ArrayList<>();
 
-        int size = 3;
-        int accesses[] = new int[]{1, 2, 3, 4};
+        int size = 4;
+        int accesses[] = new int[]{0, 3, 2, 0, 1, 3, 4, 3, 1, 0, 3, 2, 1, 3, 4};
 
         for (int access : accesses) {
-            if (pages.contains(access)) {
-                pages.remove(access);
-                pages.add(access);
-            } else {
-                if (pages.size() == size) {
-                    pages.remove(0);
-                }
-                pages.add(access);
+            if (pages.contains(new Integer(access))) {
+                pages.remove(new Integer(access));
+            } else if (pages.size() == size) {
+                pages.remove(0);
             }
+            pages.add(new Integer(access));
             System.out.println(pages);
         }
     }
