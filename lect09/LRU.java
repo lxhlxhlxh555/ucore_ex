fa08616a544d07b090411b54814c7345751defdb
lxh <86849081@qq.com>
2016-03-21 09:46:45 +0800

    java lru

---
+++ lect09/LRU.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LRU {
+    public static void main(String[] args) {
+        List<Integer> pages = new ArrayList<>();
+
+        int size = 3;
+        int accesses[] = new int[]{1, 2, 3, 4};
+
+        for (int access : accesses) {
+            boolean exist = false;
+            if (pages.contains(access)) {
+                pages.remove(access);
+                pages.add(access);
+                exist = true;
+            }
+            if (!exist) {
+                if (pages.size() == size) {
+                    pages.remove(0);
+                }
+                pages.add(access);
+            }
+            System.out.println(pages);
+        }
+    }
+}
